0e19009667263224905d4bc893c6017472501010
Javan Luiz <144359052+DevJavanLuiz@users.noreply.github.com>
2023-10-18 13:44:37 -0300

    Add files via upload

---
+++ aula1/somaresultado.js
@@ -0,0 +1,11 @@
+let primeiroNumero = prompt('Digite o primeiro numero:')
+
+let segundoNumero = prompt('Digite o segundo numero:') 
+
+
+primeiroNumero = Number(primeiroNumero)
+segundoNumero = Number(segundoNumero)
+
+let resultado = primeiroNumero + segundoNumero
+
+alert(`Resultado somado:${primeiroNumero + segundoNumero}`)
